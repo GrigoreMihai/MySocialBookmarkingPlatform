96d05559304a6c5c869656f8b748674a37ba13fd
GrigoreMihai <grigoremihai56@gmail.com>
2019-12-07 13:41:43 +0200

    chore: you know what

--- MyPinterestVersion/App_Start/Startup.Auth.cs
+++ MyPinterestVersion/App_Start/Startup.Auth.cs
@@ -54,15 +54,10 @@ namespace MyPinterestVersion
             //   consumerKey: "",
             //   consumerSecret: "");
 
-<<<<<<< Updated upstream
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
-=======
            // app.UseFacebookAuthentication(
             //   appId: "",
             //   appSecret: ""); //uncoment and add stuff but dont push to git 
->>>>>>> Stashed changes
+
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
