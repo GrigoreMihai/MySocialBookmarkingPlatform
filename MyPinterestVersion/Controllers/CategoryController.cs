1ca4f06566c4131ea171345f0190729d193a711f
GrigoreMihai <grigoremihai56@gmail.com>
2020-01-04 20:26:43 +0200

    build: categories manage for admin

---
+++ MyPinterestVersion/Controllers/CategoryController.cs
@@ -0,0 +1,108 @@
+﻿using MyPinterestVersion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPinterestVersion.Controllers
+{
+
+    [Authorize(Roles = "Administrator")]
+    public class CategoriesController : Controller
+    {
+        private ApplicationDbContext db = ApplicationDbContext.Create();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
+
+            var categories = from category in db.Categories
+                             orderby category.Name
+                             select category;
+            ViewBag.Categories = categories;
+            return View();
+        }
+
+        public ActionResult Show(int id)
+        {
+            Category category = db.Categories.Find(id);
+            return View(category);
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(Category cat)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Categories.Add(cat);
+                    db.SaveChanges();
+                    TempData["message"] = "Categoria a fost adaugata!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(cat);
+                }
+            }
+            catch (Exception e)
+            {
+                return View(cat);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Category category = db.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int id, Category requestCategory)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Category category = db.Categories.Find(id);
+                    if (TryUpdateModel(category))
+                    {
+                        category.Name = requestCategory.Name;
+                        TempData["message"] = "Categoria a fost modificata!";
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(requestCategory);
+                }
+            }
+            catch (Exception e)
+            {
+                return View(requestCategory);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            TempData["message"] = "Categoria a fost stearsa!";
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
